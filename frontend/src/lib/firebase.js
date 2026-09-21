678a8802dc59fd72918b5828bfc76da445691731
Mohammad Arju <mudassiransari8322@gmail.com>
2026-09-21 19:48:32 +0530

    Env file change

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -6,13 +6,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "aichatbot-b581b.firebaseapp.com",
+  projectId: "aichatbot-b581b",
+  storageBucket: "aichatbot-b581b.firebasestorage.app",
+  messagingSenderId: "913919760455",
+  appId: "1:913919760455:web:eab49ef269052222087962",
+  measurementId: "G-K203ZSZWFZ",
 };
 
 // Initialize Firebase
